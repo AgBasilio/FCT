9459f9d7b9cda3b12254fdb86fae77145c3211c5
fj <fabricio17j@gmail.com>
2020-05-12 10:20:39 +0200

    tabs

---
+++ ProyectoIncremental/app/src/main/java/com/example/proyectoincremental/ui/home/PagerController.java
@@ -0,0 +1,4 @@
+package com.example.proyectoincremental.ui.home;
+
+class PagerController {
+}
---
+++ ProyectoIncremental/app/src/main/java/com/example/proyectoincremental/ui/home/primero.java
@@ -0,0 +1,29 @@
+package com.example.proyectoincremental.ui.home;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.proyectoincremental.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class primero extends Fragment {
+
+    public primero() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_primero, container, false);
+    }
+}
