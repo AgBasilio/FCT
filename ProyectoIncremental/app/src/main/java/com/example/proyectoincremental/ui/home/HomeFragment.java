469ba5a03abc297c45fa3c0c176b7d30187e1d04
fj <fabricio17j@gmail.com>
2020-05-12 19:35:11 +0200

    comitfabri

--- ProyectoIncremental/app/src/main/java/com/example/proyectoincremental/ui/home/HomeFragment.java
+++ ProyectoIncremental/app/src/main/java/com/example/proyectoincremental/ui/home/HomeFragment.java
@@ -11,25 +11,29 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.proyectoincremental.R;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class HomeFragment extends Fragment {
-
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    TabItem uno, dos, tres;
+    PagerController pagerController;
     private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        viewPager = (ViewPager) view.findViewById(R.id.viewpage);
+        tabLayout = (TabLayout) view.findViewById(R.id.tab);
+        uno = (TabItem) view.findViewById(R.id.uno);
+        dos = (TabItem) view.findViewById(R.id.dos);
+        tres = (TabItem) view.findViewById(R.id.tres);
+
+        return view;
     }
 }
--- ProyectoIncremental/app/src/main/java/com/example/proyectoincremental/ui/home/PagerController.java
+++ ProyectoIncremental/app/src/main/java/com/example/proyectoincremental/ui/home/PagerController.java
@@ -1,4 +1,34 @@
 package com.example.proyectoincremental.ui.home;
 
-class PagerController {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+class PagerController extends FragmentPagerAdapter {
+    int num;
+
+    public PagerController(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+        this.num = behavior;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+            case 1:
+            case 2:
+            default:
+
+                return null;
+
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return num;
+    }
 }
