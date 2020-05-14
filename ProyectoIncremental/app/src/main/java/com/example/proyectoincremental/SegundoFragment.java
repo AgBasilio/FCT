2d0caa8361c60e01cba899e724b1fafd44da4c4f
fj <fabricio17j@gmail.com>
2020-05-14 12:21:38 +0200

    prueba123

--- ProyectoIncremental/app/src/main/java/com/example/proyectoincremental/SegundoFragment.java
+++ ProyectoIncremental/app/src/main/java/com/example/proyectoincremental/SegundoFragment.java
@@ -23,6 +23,6 @@ public class SegundoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_segundo, container, false);
+        return inflater.inflate(R.layout.fragment_slideshow, container, false);
     }
 }
