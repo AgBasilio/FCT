c4a20d985511ce18985696b08d92df23b3b76789
fj <fabricio17j@gmail.com>
2020-05-19 08:47:43 +0200

    crear modelos de aignatura y grupos

---
+++ ProyectoIncremental/app/src/main/java/com/example/proyectoincremental/Utils/Asignatura.java
@@ -0,0 +1,41 @@
+package com.example.proyectoincremental.Utils;
+
+public class Asignatura {
+    String nombre;
+    String curso;
+    String descricion;
+    String imgAsignatura;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getDescricion() {
+        return descricion;
+    }
+
+    public void setDescricion(String descricion) {
+        this.descricion = descricion;
+    }
+
+    public String getImgAsignatura() {
+        return imgAsignatura;
+    }
+
+    public void setImgAsignatura(String imgAsignatura) {
+        this.imgAsignatura = imgAsignatura;
+    }
+}
---
+++ ProyectoIncremental/app/src/main/java/com/example/proyectoincremental/Utils/Grupos.java
@@ -0,0 +1,23 @@
+package com.example.proyectoincremental.Utils;
+
+public class Grupos {
+
+    String nombreGrupo;
+    int numeroGrupo;
+
+    public String getNombreGrupo() {
+        return nombreGrupo;
+    }
+
+    public void setNombreGrupo(String nombreGrupo) {
+        this.nombreGrupo = nombreGrupo;
+    }
+
+    public int getNumeroGrupo() {
+        return numeroGrupo;
+    }
+
+    public void setNumeroGrupo(int numeroGrupo) {
+        this.numeroGrupo = numeroGrupo;
+    }
+}
